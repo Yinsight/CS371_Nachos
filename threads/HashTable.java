2b8be27d037f00da983e24a3ca61c32c798b3006
Yunting Yin <heatheryin1213@gmail.com>
2018-10-02 17:14:53 -0700

    Concurrent Hashtable Updated

--- threads/HashTable.java
+++ threads/HashTable.java
@@ -4,19 +4,17 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.Semaphore;
 import nachos.threads.LinkedList;
 import nachos.threads.LinkedList.Node;
 
 public class HashTable {
-
+	
 	enum OperationType {
-		INSERT,
-		REMOVE,
-		QUERY
+		INSERT, REMOVE, QUERY
 	}
 
-	class ThreadOperation
-	{
+	class ThreadOperation {
 		int k;
 		OperationType op;
 		int result;
@@ -31,17 +29,18 @@ public class HashTable {
 		public Key() {
 		}
 	}
-	
+
 	private int count;
+	Semaphore countSem;
 	private final int lengthOfTable;
 	private Key[] keys;
 	private ExecutorService pool;
+
 	public HashTable(int n) {
-	
 		count = 0;
+		countSem = new Semaphore(1);
 		lengthOfTable = n;
 		keys = new Key[n];
-
 		for (int i = 0; i < n; i++) {
 			keys[i] = new Key();
 		}
@@ -53,7 +52,6 @@ public class HashTable {
 	}
 
 	public void insert(int k, int val) {
-
 		System.out.println("insert");
 		k = hash(k);
 		if (k < 0 || k >= lengthOfTable) {
@@ -62,38 +60,44 @@ public class HashTable {
 		}
 		try {
 			keys[k].list.add(val);
+			countSem.acquire();
 			count++;
+			countSem.release();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void remove(int k) {
-
 		System.out.println("remove");
 		int key = k;
 		k = hash(k);
-		
 		if (k < 0 || k >= lengthOfTable) {
 			System.err.println("wrong K");
 			throw new IllegalArgumentException("wrong K");
 		}
-
 		LinkedList list = keys[k].list;
-		list.remove(key);
+		if (list.findByValue(key) > 0) {
+			list.remove(key);
+			try {
+				countSem.acquire();
+				count--;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				countSem.release();
+			}
+		}
 	}
 
 	public int get(int k) throws Exception {
-
 		System.out.println("get");
 		int key = k;
 		k = hash(k);
-
 		if (k < 0 || k >= lengthOfTable) {
 			System.err.println("wrong K");
 			throw new IllegalArgumentException("wrong K");
 		}
-
 		if (keys[k].list.findByValue(key) != -1) {
 			return key;
 		}
@@ -105,27 +109,20 @@ public class HashTable {
 	}
 
 	public void batch(int n, ThreadOperation[] ops) {
-
 		Future[] futures = new Future[n];
 		int i = 0;
-
 		for (final ThreadOperation threadOperation : ops) {
 			switch (threadOperation.op) {
 			case INSERT:
 				futures[i++] = pool.submit(new Runnable() {
-					
 					@Override
 					public void run() {
 						insert(threadOperation.k, threadOperation.result);
 					}
 				});
-
 				break;
-
 			case QUERY:
-
 				futures[i++] = pool.submit(new Runnable() {
-
 					@Override
 					public void run() {
 						try {
@@ -134,17 +131,12 @@ public class HashTable {
 						} catch (Exception e) {
 							e.printStackTrace();
 						} finally {
-
 						}
 					}
 				});
-
 				break;
-
 			case REMOVE:
-
 				futures[i++] = pool.submit(new Runnable() {
-
 					@Override
 					public void run() {
 						try {
@@ -152,19 +144,14 @@ public class HashTable {
 						} catch (Exception e) {
 							e.printStackTrace();
 						} finally {
-
 						}
 					}
 				});
-
 				break;
-
 			default:
-
 				break;
 			}
 		}
-
 		try {
 			for (Future item : futures) {
 				if (item != null) {
@@ -172,25 +159,20 @@ public class HashTable {
 					System.out.println("---future.idDone()" + item.isDone());
 				}
 			}
-
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (ExecutionException e) {
 			e.printStackTrace();
 		} finally {
-
 		}
 	}
 
 	public static void main(String[] args) {
-
 		HashTable table = new HashTable(10);
 		ThreadOperation[] ops = new ThreadOperation[5];
-
 		for (int i = 0; i < 5; i++) {
 			ops[i] = table.getThreadOperation();
 		}
-
 		ops[0].k = 11;
 		ops[0].op = OperationType.INSERT;
 		ops[0].result = 9;
@@ -206,7 +188,7 @@ public class HashTable {
 		ops[4].k = 24;
 		ops[4].op = OperationType.REMOVE;
 		ops[4].result = 1;
-		table.batch(5, ops);	
+		table.batch(5, ops);
 		ThreadOperation[] ops2 = new ThreadOperation[1];
 		ops2[0] = table.getThreadOperation();
 		ops2[0].k = 14;
