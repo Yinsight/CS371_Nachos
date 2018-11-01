24d71391ae3bb338ccf90e6cfe62938146f4c694
Krutika Wadhwa <41032373+krutikaw@users.noreply.github.com>
2018-11-01 12:09:26 -0400

    Update Condition.java

--- threads/Condition.java
+++ threads/Condition.java
@@ -1,21 +1,112 @@
 package nachos.threads;
 
-import java.util.LinkedList;
-
 import nachos.machine.*;
 
-
+import java.util.LinkedList;
 
 /**
- * An implementation of condition variables that disables interrupt()s for
- * synchronization.
+ * An implementation of condition variables built upon semaphores.
  *
  * <p>
- * You must implement this.
+ * A condition variable is a synchronization primitive that does not have
+ * a value (unlike a semaphore or a lock), but threads may still be queued.
  *
- * @see	nachos.threads.Condition
+ * <p><ul>
+ *
+ * <li><tt>sleep()</tt>: atomically release the lock and relinkquish the CPU
+ * until woken; then reacquire the lock.
+ *
+ * <li><tt>wake()</tt>: wake up a single thread sleeping in this condition
+ * variable, if possible.
+ *
+ * <li><tt>wakeAll()</tt>: wake up all threads sleeping inn this condition
+ * variable.
+ *
+ * </ul>
+ *
+ * <p>
+ * Every condition variable is associated with some lock. Multiple condition
+ * variables may be associated with the same lock. All three condition variable
+ * operations can only be used while holding the associated lock.
+ *
+ * <p>
+ * In Nachos, condition variables are summed to obey <i>Mesa-style</i>
+ * semantics. When a <tt>wake()</tt> or <tt>wakeAll()</tt> wakes up another
+ * thread, the woken thread is simply put on the ready list, and it is the
+ * responsibility of the woken thread to reacquire the lock (this reacquire is
+ * taken core of in <tt>sleep()</tt>).
+ *
+ * <p>
+ * By contrast, some implementations of condition variables obey
+ * <i>Hoare-style</i> semantics, where the thread that calls <tt>wake()</tt>
+ * gives up the lock and the CPU to the woken thread, which runs immediately
+ * and gives the lock and CPU back to the waker when the woken thread exits the
+ * critical section.
+ *
+ * <p>
+ * The consequence of using Mesa-style semantics is that some other thread
+ * can acquire the lock and change data structures, before the woken thread
+ * gets a chance to run. The advance to Mesa-style semantics is that it is a
+ * lot easier to implement.
  */
-public class Condition2 {
+public class Condition {
+	
+	 public static void cvTest5() {
+	        final Lock lock = new Lock();
+	        // final Condition empty = new Condition(lock);
+	        final Condition2 empty = new Condition2(lock);
+	        final LinkedList<Integer> list = new LinkedList<Integer>();
+
+	        KThread consumer = new KThread( new Runnable () {
+	                public void run() {
+	                    lock.acquire();
+	                    while(list.isEmpty()){
+	                        
+									empty.sleep();
+								
+							
+								
+							
+	                    }
+	                    Lib.assertTrue(list.size() == 5, "List should have 5 values.");
+	                    while(!list.isEmpty()) {
+	                        // context swith for the fun of it
+	                        KThread.currentThread().yield();
+	                        System.out.println("Removed " + list.removeFirst());
+	                    }
+	                    lock.release();
+	                }
+	            });
+
+	        KThread producer = new KThread( new Runnable () {
+	                public void run() {
+	                    lock.acquire();
+	                    for (int i = 0; i < 5; i++) {
+	                        list.add(i);
+	                        System.out.println("Added " + i);
+	                        // context swith for the fun of it
+	                        KThread.currentThread().yield();
+	                    }
+	                    empty.wake();
+	                    lock.release();
+	                }
+	            });
+
+	        consumer.setName("Consumer");
+	        producer.setName("Producer");
+	        consumer.fork();
+	        producer.fork();
+
+	        // We need to wait for the consumer and producer to finish,
+	        // and the proper way to do so is to join on them.  For this
+	        // to work, join must be implemented.  If you have not
+	        // implemented join yet, then comment out the calls to join
+	        // and instead uncomment the loop with yield; the loop has the
+	        // same effect, but is a kludgy way to do it.
+	        consumer.join();
+	        producer.join();
+	        //for (int i = 0; i < 50; i++) { KThread.currentThread().yield(); }
+	    }
     /**
      * Allocate a new condition variable.
      *
@@ -24,9 +115,10 @@ public class Condition2 {
      *				lock whenever it uses <tt>sleep()</tt>,
      *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
      */
-    public Condition2(Lock conditionLock) {
+    public Condition(Lock conditionLock) {
 	this.conditionLock = conditionLock;
-	
+
+	waitQueue = new LinkedList<Semaphore>();
     }
 
     /**
@@ -34,20 +126,22 @@ public class Condition2 {
      * variable until another thread wakes it using <tt>wake()</tt>. The
      * current thread must hold the associated lock. The thread will
      * automatically reacquire the lock before <tt>sleep()</tt> returns.
-     * @throws InterruptedException 
+     *
+     * <p>
+     * This implementation uses semaphores to implement this, by allocating a
+     * semaphore for each waiting thread. The waker will <tt>V()</tt> this
+     * semaphore, so thre is no chance the sleeper will miss the wake-up, even
+     * though the lock is released before caling <tt>P()</tt>.
      */
-    public void sleep()  {
+    public void sleep() {
 	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-	boolean intStat=Machine.interrupt().disable();
+	Semaphore waiter = new Semaphore(0);
+	waitQueue.add(waiter);
+
 	conditionLock.release();
-	
-	
-	waitQueue.add(KThread.currentThread());
-	KThread.sleep();
-	
-	conditionLock.acquire();
-	Machine.interrupt().restore(intStat);
+	waiter.P();
+	conditionLock.acquire();	
     }
 
     /**
@@ -56,14 +150,9 @@ public class Condition2 {
      */
     public void wake() {
 	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-	
-	boolean intStat =Machine.interrupt().disable();
-	if (!waitQueue.isEmpty()){
-		
-	    (waitQueue.removeFirst()).ready();
-	    
-	    }
-	Machine.interrupt().restore(intStat);
+
+	if (!waitQueue.isEmpty())
+	    ((Semaphore) waitQueue.removeFirst()).V();
     }
 
     /**
@@ -72,117 +161,17 @@ public class Condition2 {
      */
     public void wakeAll() {
 	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+
 	while (!waitQueue.isEmpty())
-		wake();
+	    wake();
     }
-    
+
     private Lock conditionLock;
-    private LinkedList<KThread>  waitQueue = new LinkedList<KThread>();
+    private LinkedList<Semaphore> waitQueue;
     
-    private static class InterlockTest {
-        private static Lock lock;
-        private static Condition2 cv;
-
-        private static class Interlocker implements Runnable {
-            public void run () {
-                lock.acquire();
-                for (int i = 0; i < 10; i++) {
-                    System.out.println(KThread.currentThread().getName());
-                    cv.wake();   // signal
-                   
-						cv.sleep();
-					  // wait
-                }
-                lock.release();
-            }
-        }
-
-        public InterlockTest () {
-            lock = new Lock();
-            cv = new Condition2(lock);
-
-            KThread ping = new KThread(new Interlocker());
-            ping.setName("ping");
-            KThread pong = new KThread(new Interlocker());
-            pong.setName("pong");
-
-            ping.fork();
-            pong.fork();
-
-            // We need to wait for ping to finish, and the proper way
-            // to do so is to join on ping.  (Note that, when ping is
-            // done, pong is sleeping on the condition variable; if we
-            // were also to join on pong, we would block forever.)
-            // For this to work, join must be implemented.  If you
-            // have not implemented join yet, then comment out the
-            // call to join and instead uncomment the loop with
-            // yields; the loop has the same effect, but is a kludgy
-            // way to do it.
-            ping.join();
-            // for (int i = 0; i < 50; i++) { KThread.currentThread().yield(); }
-        }
-        
-    }   
-    public static void cvTest5() {
-        final Lock lock = new Lock();
-        // final Condition empty = new Condition(lock);
-        final Condition2 empty = new Condition2(lock);
-        final LinkedList<Integer> list = new LinkedList<Integer>();
-
-        KThread consumer = new KThread( new Runnable () {
-                public void run() {
-                    lock.acquire();
-                    while(list.isEmpty()){
-                        
-							empty.sleep();
-						
-                    }
-                    Lib.assertTrue(list.size() == 5, "List should have 5 values.");
-                    while(!list.isEmpty()) {
-                        // context swith for the fun of it
-                        KThread.currentThread().yield();
-                        System.out.println("Removed " + list.removeFirst());
-                    }
-                    lock.release();
-                }
-            });
-
-        KThread producer = new KThread( new Runnable () {
-                public void run() {
-                    lock.acquire();
-                    for (int i = 0; i < 5; i++) {
-                        list.add(i);
-                        System.out.println("Added " + i);
-                        // context swith for the fun of it
-                        KThread.currentThread().yield();
-                    }
-                    empty.wake();
-                    lock.release();
-                }
-            });
-
-        consumer.setName("Consumer");
-        producer.setName("Producer");
-        consumer.fork();
-        producer.fork();
-
-        // We need to wait for the consumer and producer to finish,
-        // and the proper way to do so is to join on them.  For this
-        // to work, join must be implemented.  If you have not
-        // implemented join yet, then comment out the calls to join
-        // and instead uncomment the loop with yield; the loop has the
-        // same effect, but is a kludgy way to do it.
-        consumer.join();
-        producer.join();
-        //for (int i = 0; i < 50; i++) { KThread.currentThread().yield(); }
-    }
-
-    // Invoke Condition2.selfTest() from ThreadedKernel.selfTest()
-
     public static void selfTest() {
         //new InterlockTest();
     	cvTest5();
     }
-    
     
 }
