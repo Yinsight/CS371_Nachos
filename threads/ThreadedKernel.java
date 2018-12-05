edfe08945ca3260303441f409a9a2b14cbae8921
Krutika Wadhwa <41032373+krutikaw@users.noreply.github.com>
2018-12-04 21:58:15 -0500

    Update ThreadedKernel.java

--- threads/ThreadedKernel.java
+++ threads/ThreadedKernel.java
@@ -50,6 +50,7 @@ public class ThreadedKernel extends Kernel {
 	KThread.selfTest();
 	Semaphore.selfTest();
 	SynchList.selfTest();
+	PrioritySchedulerTest.simplePrioritySchedulerTest();
 	BridgeCrossing.selfTest();
 	if (Machine.bank() != null) {
 	    ElevatorBank.selfTest();
