30ee981e3991757153e7408c0f4293ec5d5642ab
Yunting Yin <heatheryin1213@gmail.com>
2018-12-07 06:24:10 -0800

    Delete loop

--- threads/HashTable.java
+++ threads/HashTable.java
@@ -136,33 +136,16 @@ public class HashTable {
 			array[i] = operation;
 		}
 		
+		/*
 		for (int j=0;j<n_ops;j++){
 			KThread array[] = new KThread[n_ops];
 			array[j].join();
 		}
+		*/
 		
 		
 	}		
 	
-	/*
-	public static class HashTableTest{
-	
-		public static void selfTest() throws LinkedListException {
-		
-			int n_ops = 100;
-		
-			ThreadOperation[] ops = new ThreadOperation[n_ops];
-			
-			for(int i=0;i<n_ops;i++){
-				ops[i] = new ThreadOperation(i, OperationType.INSERT, i);
-			
-			}
-			HashTable table = new HashTable(1000);
-			table.batch(n_ops, ops);
-		}	
-	}
-	
-	*/
 	
 	public int getNumElement(){
 		int sum = 0;
