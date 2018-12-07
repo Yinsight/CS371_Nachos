1466f0a57de03d7fd8ca91d1cb17e04d24f178bc
Yunting Yin <heatheryin1213@gmail.com>
2018-12-07 15:56:57 -0800

    Assignment 4 Submission

--- userprog/UserKernel.java
+++ userprog/UserKernel.java
@@ -1,6 +1,5 @@
 package nachos.userprog;
 
-
 import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
@@ -15,175 +14,150 @@ import java.util.Stack;
  * A kernel that can support multiple user processes.
  */
 public class UserKernel extends ThreadedKernel {
-    /**
-     * Allocate a new user kernel.
-     */
-	
-    public UserKernel() {
-	super();
-	for(int i=0;i<Machine.processor().getNumPhysPages();i++){
-		freePages.put(i,0);
+	/**
+	 * Allocate a new user kernel.
+	 */
+
+	public UserKernel() {
+		super();
+		for (int i = 0; i < Machine.processor().getNumPhysPages(); i++) {
+			freePages.put(i, 0);
+		}
 	}
-    }
 
-    /**
-     * Initialize this kernel. Creates a synchronized console and sets the
-     * processor's exception handler.
-     */
-    public void initialize(String[] args) {
-	super.initialize(args);
+	/**
+	 * Initialize this kernel. Creates a synchronized console and sets the
+	 * processor's exception handler.
+	 */
+	public void initialize(String[] args) {
+		super.initialize(args);
 
-	console = new SynchConsole(Machine.console());
-	
-	Machine.processor().setExceptionHandler(new Runnable() {
-		public void run() { exceptionHandler(); }
-	    });
-    }
+		console = new SynchConsole(Machine.console());
 
-    /**
-     * Test the console device.
-     */	
-    public void selfTest() {
-	super.selfTest();
-
-	System.out.println("Testing the console device. Typed characters");
-	System.out.println("will be echoed until q is typed.");
-
-	char c;
-
-	do {
-	    c = (char) console.readByte(true);
-	    console.writeByte(c);
+		Machine.processor().setExceptionHandler(new Runnable() {
+			public void run() {
+				exceptionHandler();
+			}
+		});
 	}
-	while (c != 'q');
 
-	System.out.println("");
-    }
+	/**
+	 * Test the console device.
+	 */
+	public void selfTest() {
+		super.selfTest();
 
-    /**
-     * Returns the current process.
-     *
-     * @return	the current process, or <tt>null</tt> if no process is current.
-     */
-    public static UserProcess currentProcess() {
-	if (!(KThread.currentThread() instanceof UThread))
-	    return null;
-	
-	return ((UThread) KThread.currentThread()).process;
-    }
+		System.out.println("Testing the console device. Typed characters");
+		System.out.println("will be echoed until q is typed.");
 
-    public static int getFreePage(){
-    	return getFreePages(1)[0];
-    	
-    }
-    
-    public static int[] getFreePages(int numOfPages){
-    	
-    	int[] availablePage= new int[numOfPages];
-    	int i=0;
-    	
-<<<<<<< HEAD
-    	while(i<Machine.processor().getNumPhysPages()&&numOfPages>0){
-=======
-    	while(i<Machine.processor().getNumPhysPages()&& numOfPages>0){
-    		
->>>>>>> c583976691a551e2d4efc39abb20458546a80c0c
-    		
-    			lock.acquire();
-    			if(freePages.containsKey(i)){
-    			availablePage[numOfPages]=i;
-    			freePages.remove(i);
-    			numOfPages--;
-    			}
-    			i++;
-    			lock.release();
-<<<<<<< HEAD
-    	
-=======
-    		
->>>>>>> c583976691a551e2d4efc39abb20458546a80c0c
-    	}
-    	return availablePage;
-    	
-    }
-    
-    public static void free(int A[]){
-    	for(int i=0;i<A.length;i++){
-    		Machine.interrupt().disable();
-    		freePages.put(A[i],0);
-    		Machine.interrupt().enable();
-    	}
-    }
-    /**
-     * The exception handler. This handler is called by the processor whenever
-     * a user instruction causes a processor exception.
-     *
-     * <p>
-     * When the exception handler s invoked, interrupts are enabled, and the
-     * processor's cause register contains an integer identifying the cause of
-     * the exception (see the <tt>exceptionZZZ</tt> constants in the
-     * <tt>Processor</tt> class). If the exception involves a bad virtual
-     * address (e.g. page fault, TLB miss, read-only, bus error, or address
-     * error), the processor's BadVAddr register identifies the virtual address
-     * that caused the exception.
-     */
-    public void exceptionHandler() {
-	Lib.assertTrue(KThread.currentThread() instanceof UThread);
+		char c;
 
-	UserProcess process = ((UThread) KThread.currentThread()).process;
-	int cause = Machine.processor().readRegister(Processor.regCause);
-	process.handleException(cause);
-    }
+		do {
+			c = (char) console.readByte(true);
+			console.writeByte(c);
+		} while (c != 'q');
 
-    
-    
-    /**
-     * Start running user programs, by creating a process and running a shell
-     * program in it. The name of the shell program it must run is returned by
-     * <tt>Machine.getShellProgramName()</tt>.
-     *
-     * @see	nachos.machine.Machine#getShellProgramName
-     */
-    public void run() {
-	super.run();
+		System.out.println("");
+	}
 
-	UserProcess process = UserProcess.newUserProcess();
-	
-	String shellProgram = Machine.getShellProgramName();	
-	Lib.assertTrue(process.execute(shellProgram, new String[] { }));
+	/**
+	 * Returns the current process.
+	 * 
+	 * @return the current process, or <tt>null</tt> if no process is current.
+	 */
+	public static UserProcess currentProcess() {
+		if (!(KThread.currentThread() instanceof UThread))
+			return null;
 
-	KThread.currentThread().finish();
-    }
+		return ((UThread) KThread.currentThread()).process;
+	}
 
-    /**
-     * Terminate this kernel. Never returns.
-     */
-    public void terminate() {
-	super.terminate();
-    }
+	public static int getFreePage() {
+		return getFreePages(1)[0];
 
-    /** Globally accessible reference to the synchronized console. */
-    public static SynchConsole console;
-    
-<<<<<<< HEAD
-    //protected TranslationEntry[] pageTable;
-    //initialize hash-map
-    static HashMap<Integer, Integer> freePages = new HashMap<Integer, Integer>();
-    static Lock lock = new Lock();
-=======
-    /*private static Stack<Integer> freePages = new Stack<Integer>();
-    static{
-    for(int i=0;i<Machine.processor().getNumPhysPages();i++){
-    	freePages.add(i);
-    }
-    }*/
-    
-    //protected TranslationEntry[] pageTable;
-    //initialize hash-map
-    static HashMap<Integer, Integer> freePages = new HashMap<Integer, Integer>();
-    static final Lock lock = new Lock();
->>>>>>> c583976691a551e2d4efc39abb20458546a80c0c
-    // dummy variables to make javac smarter
-    private static Coff dummy1 = null;
+	}
+
+	public static int[] getFreePages(int numOfPages) {
+
+		int[] availablePage = new int[numOfPages];
+		int i = 0;
+
+		while (i < Machine.processor().getNumPhysPages() && numOfPages > 0) {
+
+			lock.acquire();
+			if (freePages.containsKey(i)) {
+				availablePage[numOfPages] = i;
+				freePages.remove(i);
+				numOfPages--;
+			}
+			lock.release();
+
+		}
+		return availablePage;
+
+	}
+
+	public static void free(int A[]) {
+		for (int i = 0; i < A.length; i++) {
+			Machine.interrupt().disable();
+			freePages.put(A[i], 0);
+			Machine.interrupt().enable();
+		}
+	}
+
+	/**
+	 * The exception handler. This handler is called by the processor whenever a
+	 * user instruction causes a processor exception.
+	 * 
+	 * <p>
+	 * When the exception handler s invoked, interrupts are enabled, and the
+	 * processor's cause register contains an integer identifying the cause of
+	 * the exception (see the <tt>exceptionZZZ</tt> constants in the
+	 * <tt>Processor</tt> class). If the exception involves a bad virtual
+	 * address (e.g. page fault, TLB miss, read-only, bus error, or address
+	 * error), the processor's BadVAddr register identifies the virtual address
+	 * that caused the exception.
+	 */
+	public void exceptionHandler() {
+		Lib.assertTrue(KThread.currentThread() instanceof UThread);
+
+		UserProcess process = ((UThread) KThread.currentThread()).process;
+		int cause = Machine.processor().readRegister(Processor.regCause);
+		process.handleException(cause);
+	}
+
+	/**
+	 * Start running user programs, by creating a process and running a shell
+	 * program in it. The name of the shell program it must run is returned by
+	 * <tt>Machine.getShellProgramName()</tt>.
+	 * 
+	 * @see nachos.machine.Machine#getShellProgramName
+	 */
+	public void run() {
+		super.run();
+
+		UserProcess process = UserProcess.newUserProcess();
+
+		String shellProgram = Machine.getShellProgramName();
+		Lib.assertTrue(process.execute(shellProgram, new String[] {}));
+
+		KThread.currentThread().finish();
+	}
+
+	/**
+	 * Terminate this kernel. Never returns.
+	 */
+	public void terminate() {
+		super.terminate();
+	}
+
+	/** Globally accessible reference to the synchronized console. */
+	public static SynchConsole console;
+
+	// protected TranslationEntry[] pageTable;
+	// initialize hash-map
+	static HashMap<Integer, Integer> freePages = new HashMap<Integer, Integer>();
+	static Lock lock = new Lock();
+	// dummy variables to make javac smarter
+	private static Coff dummy1 = null;
 }
-
